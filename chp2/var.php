6d7513babb8079b978ff051543ec7406448d98e7
polaris65b <42924432+polaris65b@users.noreply.github.com>
2024-03-21 11:55:13 +0900

    t

---
+++ chp2/var.php
@@ -0,0 +1,16 @@
+<?php
+    $a = 10;
+    $b = 20;
+    $c = $a + $b;
+    echo "$c <br>";
+
+    $a = 18.5;
+    $b = 37.3;
+    $c = "$c <br>";
+
+    $fruit = "사과";
+    echo "$fruit <br>";
+    
+    $fruit = "오렌지";
+    echo "$fruit <br>";
+?>
